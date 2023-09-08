d78b59c8c33b802896e09479ec67edc1cedd5bfd
IG <iikim2511@gmail.com>
2023-09-08 17:41:58 -0400

    --

--- src/context/cart/CartState.js
+++ src/context/cart/CartState.js
@@ -3,12 +3,48 @@
 import React, { createContext, useReducer } from 'react'
 import CartContext from './CartContext';
 
+
+ const SHOW_HIDE_CART = "SHOW_HIDE_CART";
+ const ADD_TO_CART = "ADD_TO_CART";
+ const REMOVE_ITEM = "REMOVE_ITEM";
+
+
 const CartState = ({children}) => {
     
     // 🍀reducer
     const CartReducer = (state, action) => {
 
-
+        // switch (action.type) {    
+        //         case SHOW_HIDE_CART: {
+        //           return {
+        //             ...state,
+        //             showCart: !state.showCart, /* 🍀show & hide Btn */
+        //           };
+        //         }
+      
+        //   /* 🍀...spread operator 
+        //       + new item add */
+        //   case ADD_TO_CART: {
+        //           return {
+        //             ...state,
+        //             cartItems: [...state.cartItems, action.payload],
+        //           };
+        //         }
+      
+        //   /* 🍀remove: 
+        //     .filter
+        //     item._id 빼고 새로 리스트 만듬 */
+        //   case REMOVE_ITEM: {
+        //           return {
+        //             ...state,
+        //             cartItems: state.cartItems.filter(
+        //               (item) => item._id !== action.payload
+        //               ),
+        //             };
+        //           }
+        //   default:
+        //     return state;
+        // }
       };
     
     
@@ -16,8 +52,8 @@ const CartState = ({children}) => {
 
     const initialState ={
         showCart:false,
-        cartItems:["cart"],
-        test:"test-hello",
+        cartItems:['cartitme1'],
+        test:"test",
     }
     const [state, dispatch] = useReducer(CartReducer, initialState);
 
