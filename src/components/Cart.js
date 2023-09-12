5ad865d9533ca0e5dc064cc8d76639b1ff45dfb0
IG <iikim2511@gmail.com>
2023-09-12 17:47:49 -0400

    cartitems

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,69 +1,63 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import "./Cart.css";
 
-import CartContext from '../context/CartContext'
-import CartItems from './CartItems'
+import CartContext from "../context/CartContext";
+import CartItems from "./CartItems";
 
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 
 const Cart = () => {
+  const { showCart, cartItems, showHideCart, removeItem, totalPrice } =
+    useContext(CartContext);
 
-    
-    const {showCart, cartItems, showHideCart, totalPrice} = useContext(CartContext)
-
-    console.log(showCart)
-
-
-
+  console.log(showCart);
 
   return (
-    <div>Cart
-        {
-            showCart ? (
-                <div></div>
-            )
-            : (
-                <main className='cart__wrapper'>
-                    <div style={{ textAlign: "right" }}>
-                        <CloseIcon
-                            style={{ cursor: "pointer" }}
-                            className='Close-Icon'
-                            aria-hidden='true'
-                            onClick={showHideCart}
-                        />
+    <div>
+      Cart
+      {showCart ? (
+        <div></div>
+      ) : (
+        <main className="cart__wrapper">
+          <div style={{ textAlign: "right" }}>
+            <CloseIcon
+              style={{ cursor: "pointer" }}
+              className="Close-Icon"
+              aria-hidden="true"
+              onClick={showHideCart}
+            />
+          </div>
+          <div className="cart__innerWrapper">
+            {cartItems.length === 0 ? (
+              <h4>Cart is empty</h4>
+            ) : (
+              <ul>
+                {cartItems.map((item) => (
+                  <li className="CartItem__item">
+                    <img src={item.image} alt="" />
+                    <div>
+                      {item.name} , {item.price} $
                     </div>
-                    <div className='cart__innerWrapper'>
-                        {
-                            cartItems.length ===0 ?
-                            (<h4>Cart is empty</h4>)
-                            :(
-                                <ul>
-                                {
-                                    cartItems.map((item)=>(
-                                        <CartItems key={item.id} item={item}/>
-                                    ))
-                                }
-                                </ul>
-                            )
-                        }
-                    </div>
+                    <button
+                      className="CartItem__button"
+                      onClick={() => removeItem(item._id)}
+                    >
+                      Remove
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
 
-                    <div className='Cart__cartTotal'>
-                        <div>Total Price : </div>
-                        <div style={{marginLeft:5}}>                       
-                            {totalPrice.toFixed(2)}                        
-                        </div>
-                    </div>
-                
-                </main>
-            )
-
-
-        }
-    
+          <div className="Cart__cartTotal">
+            <div>Total Price : </div>
+            <div style={{ marginLeft: 5 }}>{totalPrice.toFixed(2)}</div>
+          </div>
+        </main>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Cart
-
+export default Cart;
